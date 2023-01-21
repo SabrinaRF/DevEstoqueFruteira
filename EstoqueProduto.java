8037ff48887bf2f4b85412023ffcd986b773bf42
sabrina <sabrinarfernandes.srf@gmail.com>
2023-01-20 21:58:42 -0300

    Criação do DevEstoqueFruteira

---
+++ EstoqueProduto.java
@@ -0,0 +1,103 @@
+package trabalhoag3.TrabalhoAG3;
+
+import java.util.ArrayList;
+import java.util.List;
+import trabalhoag3.TrabalhoAG3.Execoes.CodigoInvalidoException;
+public class EstoqueProduto implements IProduto{
+
+    List<Produto> listaProdutos;
+
+    public EstoqueProduto (List<Produto> p){
+        this.listaProdutos = p;
+    }
+    public EstoqueProduto (){
+        this.listaProdutos = new ArrayList<>();
+    }
+
+    @Override
+    public void addProduto(Produto p) throws Exception {
+        //if (p != null) {
+            listaProdutos.add(p);
+        //}else{
+            throw new Exception("Produto não pode ser adicionado!");
+        //}   
+    }
+
+    @Override 
+    public void removeProduto(int codigo) throws Exception {
+        
+        listaProdutos.remove(getProduto(codigo));
+        
+    }
+
+    @Override 
+    public Produto getProduto(int codigo) throws Exception {
+        for(int i=0;i<listaProdutos.size();i++){
+            if (listaProdutos.get(i).getCodigo() == codigo ){ 
+                return  listaProdutos.get(i) ;
+            }
+        }
+        throw new CodigoInvalidoException();
+    }
+
+    @Override
+    public void updateQuantidade(int codigo, double nova) throws Exception {
+        for(int i=0;i<listaProdutos.size();i++){
+            if (listaProdutos.get(i).getCodigo() ==codigo) {
+                listaProdutos.get(i).setQuantidade(nova);
+            }else{
+                throw new CodigoInvalidoException();
+            }
+        }
+        
+    }
+
+    @Override
+    public void updatePreco(int codigo, double novo) throws Exception {
+        for(int i=0;i<listaProdutos.size();i++){
+            if (listaProdutos.get(i).getPreco() == codigo) {
+                listaProdutos.get(i).setPreco(novo);
+            }else{
+                throw new CodigoInvalidoException();
+            }
+        }
+        
+    }
+
+    @Override
+    public void addQuantidade(int codigo, double quantidade) throws Exception {
+        for(int i=0;i<listaProdutos.size();i++){
+            if (listaProdutos.get(i).getCodigo() ==codigo) {
+                double novaQuantidade = (listaProdutos.get(i).getQuantidade())+ quantidade;
+                
+                    listaProdutos.get(i).setQuantidade(novaQuantidade);
+                
+            }else{
+                throw new CodigoInvalidoException();
+            }
+        }
+        
+    }
+
+    @Override
+    public void subQuantidade(int codigo, double quantidade) throws Exception {
+        for(int i=0;i<listaProdutos.size();i++){
+            if (listaProdutos.get(i).getCodigo() ==codigo) {
+                double novaQuantidade = (listaProdutos.get(i).getQuantidade())-quantidade;              
+                listaProdutos.get(i).setQuantidade(novaQuantidade);
+            }else{
+                throw new CodigoInvalidoException();
+            }
+        }
+    }
+
+    public String toString(){
+        String dados="";
+        for(Produto v: listaProdutos){
+            dados+=v.toString()+"\n";
+        }
+        return dados;
+       
+    }
+   
+}
---
+++ ProdutoCategoriaPeso.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package trabalhoag3.TrabalhoAG3;
+
+
+ 
+public class ProdutoCategoriaPeso extends Produto{
+        
+    public ProdutoCategoriaPeso(int quantidade, String nome, String descricao,double preco){
+    super(nome, descricao, quantidade, preco);
+    }
+
+    
+   
+    
+}
+
